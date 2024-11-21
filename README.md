7f43ea297a1474ee1e27620fdcee0a3599273bd1
ABODAIF29 <abodaifmohammed29@gmail.com>
2024-11-21 02:56:06 +0200

    Print Message App

---
+++ README.md
@@ -0,0 +1,5 @@
+# js
+- Get All Elements in HTML
+- Add click event to button
+- Get value from input field 
+- But value in p (Message) 
---
+++ main.js
@@ -0,0 +1,17 @@
+
+var myInput = document.getElementById("input");
+var myButton = document.getElementById("btn");
+var myOutput = document.getElementById("message");   
+
+
+myButton.onclick = function() {
+  // Get the value of the input field 
+  var inputValue = myInput.value; 
+
+  if (inputValue != "") {
+    // Put the value of the input field into the output field
+    myOutput.innerHTML = inputValue;
+    // Clear the input field 
+    myInput.value = "";
+  }
+}
